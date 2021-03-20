b789d2e0c2609ad6b2a86686882bea13df973e42
Marvin Kamphausen <marvin.kamphausen@icloud.com>
2021-03-20 23:11:00 +0100

    update

--- pages/search.js
+++ pages/search.js
@@ -113,8 +113,10 @@ export default function Search({ data }) {
 //takes in the data from api an provides it for the search page
 export async function getStaticProps() {
   //const hostname = "brandberg.vercel.app/api/figures"
-  const hostname = process.env.VERCEL_URL;
-  const res = await fetch(`${hostname}/api/figures`);
+  //const hostname = "https://raw.githubusercontent.com/mkamphausen/brandberg/main/data/figures.json";
+  
+  //leider konnteich die im Unterricht genutzte Methode für den Datenabruf noch nicht fehlerfrei implementieren, deshalb habe ich zu Testzwecken zunächst diese Methode gewählt
+  const res = await fetch("https://raw.githubusercontent.com/mkamphausen/brandberg/main/data/workshop1.json");
   const data = await res.json()
   
   // Pass data to the page via props
